c0a7c5f2f725265b61d655c4307107c1fa29d890
Sodacooky <523379653@qq.com>
2021-02-21 19:22:00 +0800

    可以分析指定图片链接的tags和获得原图链接

---
+++ YandereCrawler.cpp
@@ -0,0 +1,98 @@
+﻿#include "YandereCrawler.h"
+
+using namespace std;
+
+int main() {
+	App app;
+	return app.Main();
+}
+
+int App::Main() {
+	curl_global_init(CURL_GLOBAL_ALL);
+	CURL* handle = curl_easy_init();
+
+	curl_easy_setopt(handle, CURLOPT_URL, "https://yande.re/post/show/742047");
+	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, __WriteSourceCodeToString);
+	curl_easy_setopt(handle, CURLOPT_WRITEDATA, &m_strNowWebSourceCode);
+	curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0);
+
+	curl_easy_perform(handle);
+	curl_easy_cleanup(handle);
+	curl_global_cleanup();
+
+	if (m_strNowWebSourceCode.find("hakurei_reimu") != string::npos) {
+		cout << "Match!" << endl;
+	}
+
+	return 0;
+}
+
+void App::__TagsInputInterface() {
+	cout << "输入tags，以空格间隔，回车结束输入：" << endl;
+
+	string tmp;
+	getline(cin, tmp);
+
+	stringstream ss(tmp);
+	while (ss >> tmp) {
+		m_vec_strTags.push_back(tmp);
+	}
+
+	cout << "你输入了 " << m_vec_strTags.size() << " 个标签" << endl;
+}
+
+void App::__IdRangeInputInterface() {
+	while (true) {
+		cout << "输入起始的图片id: ";
+		cin >> m_unId;
+		cout << endl << "输入结束的图片id: ";
+		cin >> m_unEndId;
+
+		if (m_unId > m_unEndId) {
+			cout << "*** 输入的id不正确，起始id应小于结束id ***" << endl;
+		}
+		else {
+			break;
+		}
+	}
+}
+
+size_t App::__WriteSourceCodeToString(void* buff, size_t block_size, size_t block_amount, void* string_ptr) {
+	auto pString = (string*)string_ptr;
+	pString->append((char*)buff, block_amount * block_size);
+	return block_amount * block_size;
+}
+
+bool App::IsPageExist() {
+	if (m_strNowWebSourceCode.find("Not Found") == string::npos) {
+		return true;
+	}
+	return false;
+}
+
+std::string App::__ExtractTags() {
+	//从HTML的meta中提取
+
+	//固定的开头
+	const string left_keyword = R"(<meta property="og:description" content=")";
+	//tags信息以另一边引号结束
+
+	auto left_keyword_pos = m_strNowWebSourceCode.find(left_keyword);
+	auto tags_startpos = left_keyword_pos + left_keyword.size();
+
+	auto tags_endpos = m_strNowWebSourceCode.find("\"", tags_startpos);
+
+	return std::string(m_strNowWebSourceCode.begin() + tags_startpos, m_strNowWebSourceCode.begin() + tags_endpos);
+}
+
+std::string App::__ExtractLargeLink() {
+	//原图链接都以此开头
+	const string left_keyword = R"(https://files.yande.re/image/)";
+	auto startpos = m_strNowWebSourceCode.find(left_keyword);
+
+	//链接后都是引号
+	auto endpos = m_strNowWebSourceCode.find("\"", startpos);
+
+	return std::string(m_strNowWebSourceCode.begin() + startpos, m_strNowWebSourceCode.begin() + endpos);
+}
---
+++ YandereCrawler.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <curl/curl.h>	//from vcpkg
+
+class App {
+public:
+	int Main();
+
+private:
+	/*
+		储存的是__WriteSourceCodeToString()写入的网页源码
+		下一次curl perform前应该清空它，进行重写
+	*/
+	std::string m_strNowWebSourceCode;
+
+	//用户输入的一组tags，必须所有tag都能在源码中找到才算是命中
+	std::vector<std::string> m_vec_strTags;
+
+	//当前网页的Id，逐渐递增
+	unsigned int m_unId = 1;
+
+	//目标停止网页Id
+	unsigned int m_unEndId = 0;
+
+private:
+	//请用户输入tags
+	void __TagsInputInterface();
+
+	//请用户设置起始查找ID和结束ID
+	void __IdRangeInputInterface();
+
+	//curl写网页到字符串m_strNowWebSourceCode的callback
+	static size_t __WriteSourceCodeToString(void* buff, size_t block_size, size_t block_amount, void* string_ptr);
+
+	//判断网页可用性
+	bool IsPageExist();
+
+	//从当前m_strNowWebSourceCode源码中提取出tags字符串
+	//前提网页得不是404
+	std::string __ExtractTags();
+
+	//从当前m_strNowWebSourceCode源码中提取出大图的链接
+	//前提网页得不是404
+	std::string __ExtractLargeLink();
+};
